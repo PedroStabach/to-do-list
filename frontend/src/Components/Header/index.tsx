85383409c9c56e6ea1b523acb3dc84eeee03bf9e
PedroStabach <pedroaugustostabach3@gmail.com>
2025-10-15 19:02:00 -0300

    rotas funcionando

--- frontend/src/Components/Header/index.tsx
+++ frontend/src/Components/Header/index.tsx
@@ -1,31 +1,28 @@
 import { ReactNode } from 'react';
 import styles from './index.module.css';
-import { FiSearch } from 'react-icons/fi';
+import { useNavigate } from "react-router-dom";
 
-function HandleLogin() {
-    if (localStorage.getItem('token')) {
-        localStorage.removeItem('token');
-        window.location.href = '/'; // redireciona para a página de login
-    } else {
-        window.location.href = '/login';
-    }
-    
-}
 export function Header ( ) {
+    const navigate = useNavigate();
+    const token = localStorage.getItem("token");
+
+    function handleLogout(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    e.preventDefault();
+    localStorage.removeItem("token"); 
+    navigate("/login"); 
+  }
     return (
         <>
             <div className={styles.Header}>
                 <h1>Tasks</h1>
-                <div>
-                    <form className={styles.SearchArea}>
-                     <FiSearch size={30} className={styles.FiIcon} />
-                     <input type="text" />
-                    </form>
-                    <button onClick={HandleLogin}>{localStorage.getItem("token") 
-                    ? "Logout" 
-                    : "Login"}
-                    </button>
-                </div>
+
+                {token ? (
+                    <a href="/login" onClick={handleLogout}>
+                    Logout
+                    </a>
+                ) : (
+                    <a href="/login">Login</a>
+                )}
             </div>
         </>
     );
